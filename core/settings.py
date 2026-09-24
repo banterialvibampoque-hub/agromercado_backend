a21e6a542a157586d21592778bb89c582407bcd8
Banteri alvi bampoque <banterialvibampoque@email.com>
2026-09-24 01:27:59 +0000

    Fix: Ordre des middlewares et stockage WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -35,15 +35,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Doit être juste après SecurityMiddleware
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Ajoutez cette ligne ici
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -133,6 +134,12 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 MAILERS = {
     'default': {
